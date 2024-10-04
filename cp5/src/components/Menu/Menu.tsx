04f571b82b611a4ce5180682f223fa99d03152d4
mateuscastronappe <teteucastro31@gmail.com>
2024-10-04 09:30:04 -0300

    adicionando link no menu para acesso as pages

--- cp5/src/components/Menu/Menu.tsx
+++ cp5/src/components/Menu/Menu.tsx
@@ -5,6 +5,10 @@ export default function Menu() {
         <nav>
             <ul>
                 <li><Link href="/">Home</Link></li>
+                <li><Link href="/introducao">Introdução</Link></li>
+                <li><Link href="/teorias">Teorias</Link></li>
+                <li><Link href="/criticas">Criticas</Link></li>
+                <li><Link href="/demonstracao">Demonstração</Link></li>
             </ul>
         </nav>
   )
