c5bac53f1eef1034115536f9ee4898f7af5dc921
gabimezze <gabrielagomescezar@gmail.com>
2024-10-04 08:59:52 -0300

    Adicionando paginas

---
+++ cp5/src/components/Conteudos/Criticas.tsx
@@ -0,0 +1,17 @@
+import Cabecalho from "../Cabecalho/Cabecalho";
+import Rodape from "../Rodape/Rodape";
+
+export default function Criticas() {
+    return (
+        <div>
+            <h1>Críticas</h1>
+            <h2>Críticas e Repercussões de "Worlds in Collision"</h2>
+            <p>
+            Apesar da popularidade do livro, Velikovsky enfrentou severas críticas da comunidade científica, que considerava suas teorias não baseadas em evidências empíricas. Entre as críticas:
+            - Falta de Evidências Científicas: Os cientistas argumentaram que as ideias de Velikovsky careciam de dados observacionais sólidos e eram muitas vezes contraditórias com a astrofísica estabelecida.
+            - Reações Acadêmicas: O livro gerou debates acalorados, resultando em uma divisão entre aqueles que apoiavam suas ideias e os que defendiam a ciência tradicional.
+            - Legado Cultural: Apesar das controvérsias, Velikovsky influenciou movimentos de pensamento alternativo e popularizou a ideia de que a história da Terra pode ser mais dramática do que a ciência convencional sugere.
+            </p>
+        </div>
+    )
+}
---
+++ cp5/src/components/Conteudos/Introducao.tsx
@@ -0,0 +1,16 @@
+import Cabecalho from "../Cabecalho/Cabecalho";
+import Rodape from "../Rodape/Rodape";
+
+export default function Introducao() {
+    return (
+        <div>
+            <h1>Introdução</h1>
+            <h2>Worlds in Collision - Introdução às 
+                Ideias de Immanuel Velikovsky</h2>
+            <p>
+                Immanuel Velikovsky (1895-1979) foi um psicanalista e escritor que, em seu livro Worlds in Collision (1950), propôs teorias controversas sobre a história do sistema solar e eventos cataclísmicos na Terra. Sua principal ideia era que os planetas poderiam interagir de maneiras que afetariam profundamente a história da humanidade e a geologia do nosso planeta. Velikovsky desafiou a visão científica convencional, sugerindo que eventos astronômicos como a passagem de Vênus
+                perto da Terra teriam causado desastres naturais.
+            </p>
+        </div>
+    )
+}
