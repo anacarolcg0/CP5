3e85292565d1a73253896f900b1bcbe2c079b7a4
anacarolcg <anacarolcg0@gmail.com>
2024-10-04 08:22:18 -0300

    criando NotFound.

---
+++ cp5/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h1>404 : Not Found</h1>
+            <p>O recurso que você tentou acessar não está disponivel!</p>
+        </div>
+    )
+}
